dfce5a55a27ee0a65e0d60b8f863d1ef4df54d3d
mikael <mikael.st15@protonmail.com>
2025-01-09 17:37:18 -0300

    initial setup

--- lib/assets/palette.dart
+++ lib/assets/palette.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static Color background = const Color.fromARGB(255, 11, 10, 15);
-  static Color items = const Color.fromARGB(255, 29, 27, 39);
-  static Color dark = Color.fromARGB(255, 4, 3, 6);
-  static Color light_gray = const Color.fromARGB(255, 133, 133, 133);
-  static Color details = const Color.fromARGB(255, 55, 51, 74);
-  static Color dark_gray = const Color.fromARGB(255, 4, 3, 6);
-  static Color white = Colors.white;
-  static Color yellow = const Color.fromARGB(255, 255, 127, 0);
-  static Color transparent = Colors.transparent;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+
+  static const Color background = Color.fromARGB(255, 11, 10, 15);
+  static const Color card = Color.fromARGB(255, 20, 18, 27);
+  static const Color items = Color.fromARGB(255, 29, 27, 39);
+  static const Color comic_icon = Color.fromARGB(255, 50, 46, 67);
+  static const Color subtitles = Color.fromARGB(255, 106, 101, 134); 
+  static const Color details = Color.fromARGB(255, 228, 25, 59);
 }
